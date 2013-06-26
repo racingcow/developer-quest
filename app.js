40d5266d7b2452ebebf6431e99c9982adfa0aa8e
racingcow <racingcow@hotmail.com>
2013-06-26 00:57:56 -0500

    this is the time

--- app.js
+++ app.js
@@ -1,44 +1,10 @@
-/*
-var _express = require('express');
-var _app = _express();
-var _url = require('url');
-var _path = require('path');
-var _fs = require('fs');
-var _mime = require('mime');
-var _config = require('./config');
-
 //The world's most advanced web server code...BEHOLD!
-_app.get('/*', function(req, res){       
-    var uri = _url.parse(req.url).pathname;             
-    var filename = _path.join(process.cwd(), uri);         
-    _fs.exists(filename, function(exists){
-        if(!exists){        
-            res.writeHead(404, {'Content-Type' : 'text/plain'});        
-            res.write('Content not found');        
-            res.end();        
-            return;
-        } 
-        _fs.readFile(filename, 'binary', function(err, file){                
-            res.writeHead(200, {'Content-Type' : _mime.lookup(filename) });        
-            res.write(file, 'binary');        
-            res.end();
-        });          
-    }); 
-});
 
-_app.listen(_config.info.port);
-
-console.log('listening at ' + _config.info.port);
-*/
 var express = require('express');
 var app = express();
 app.use(express.logger());
 app.use(express.static(__dirname));
-/*
-app.get('/', function(request, response) {
-  response.sendfile('index.html');
-});
-*/
+app.use(express.static(__dirname + '/media'));
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
