b7b6aae3b63c40804bd0c708aae0c4bc7e6f21c3
racingcow <racingcow@hotmail.com>
2013-06-26 01:05:04 -0500

    this is the time

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 app.use(express.static(__dirname));
-app.use(express.static(__dirname + '/media'));
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
