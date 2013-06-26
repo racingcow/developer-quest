bd7c5ccceaef19a83f0a06b6d5d9d0ff142ce551
racingcow <racingcow@hotmail.com>
2013-06-26 00:48:07 -0500

    again!

--- app.js
+++ app.js
@@ -30,15 +30,17 @@ _app.listen(_config.info.port);
 
 console.log('listening at ' + _config.info.port);
 */
-var express = require("express");
+var express = require('express');
 var app = express();
 app.use(express.logger());
-
+app.use(express.static());
+/*
 app.get('/', function(request, response) {
   response.sendfile('index.html');
 });
+*/
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+  console.log('Listening on ' + port);
 });
