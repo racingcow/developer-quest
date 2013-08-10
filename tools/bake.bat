d3c7620805803fa991f5b66b7418f6cfcd611413
David Miller <racingcow@hotmail.com>
2013-08-10 14:32:45 -0500

    initial work on automated deployment

--- tools/bake.bat
+++ tools/bake.bat
@@ -2,10 +2,10 @@
 
 :: Path to impact.js and your game's main .js
 SET IMPACT_LIBRARY=lib/impact/impact.js
-SET GAME=lib/game/main.js
+SET GAME=lib/devquest/main.js
 
 :: Output file
-SET OUTPUT_FILE=game.min.js
+SET OUTPUT_FILE=../developer-quest-publish/devquest.min.js
 
 
 :: Change CWD to Impact's base dir
@@ -13,11 +13,11 @@ cd ../
 
 
 :: Bake!
-php tools/bake.php %IMPACT_LIBRARY% %GAME% %OUTPUT_FILE%
+::php tools/bake.php %IMPACT_LIBRARY% %GAME% %OUTPUT_FILE%
 
 :: If you dont have the php.exe in your PATH uncomment the
 :: following line and point it to your php.exe
 
-::c:/php/php.exe tools/bake.php %IMPACT_LIBRARY% %GAME% %OUTPUT_FILE%
+"c:\Program Files (x86)\PHP\v5.4\php.exe" tools/bake.php %IMPACT_LIBRARY% %GAME% %OUTPUT_FILE%
 
 pause
